75cf39f766c5624b4769c21f71320dd380f18313
user <user@users-MacBook-Pro-4.local>
2019-03-05 20:45:13 -0600

    Add routes and static pages controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
   def test #is this basically a method? yeah boi
-    render "test.html.erb"
+  end
+
+  def home
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get '/test' => 'static_pages#test'
-
+  get '/' => 'static_pages#home'
+  get '/home' => 'static_pages#home'
 end
